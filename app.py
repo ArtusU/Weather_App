fde56e58df3c0e249dae792c9f9d02038de83ba7
ArtusU <artusrock@hotmail.com>
2020-07-10 11:58:18 +0100

    removing duplicates cities

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
 
 db = SQLAlchemy(app)
 
@@ -12,16 +13,8 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
-@app.route('/', methods={'GET', 'POST'})
-def index():
-    if request.method == 'POST':
-        new_city = request.form.get('city')
-        
-        if new_city:
-            new_city_obj = City(name=new_city)
-            db.session.add(new_city_obj)
-            db.session.commit()
-
+@app.route('/', methods={'GET'})
+def index_get():
 
     cities = City.query.all()
 
@@ -41,3 +34,22 @@ def index():
         weather_data.append(weather)
 
     return render_template('weather.html', weather_data = weather_data)
+
+
+@app.route('/', methods={'POST'})
+def index_post():
+    err_msg = ''
+    new_city = request.form.get('city')
+    
+    if new_city:
+        existing_city = City.query.filter_by(name=new_city).first()
+        if not existing_city:
+            new_city_obj = City(name=new_city)
+            db.session.add(new_city_obj)
+            db.session.commit()
+        else:
+            err_msg = 'City already exists!'
+
+
+
+    return redirect(url_for('index_get'))
