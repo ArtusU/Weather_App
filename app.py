d1097f10a7ececddebbdc89e0c310d2f008d5dda
ArtusU <artusrock@hotmail.com>
2020-07-10 12:55:41 +0100

    non-existing city

--- app.py
+++ app.py
@@ -13,22 +13,29 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
+
+def get_weather_data(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }&units=metric&appid=d645341da3d5cd09006398538afc96c0'
+    r = requests.get(url).json()
+    return r
+ 
+
 @app.route('/', methods={'GET'})
 def index_get():
-
     cities = City.query.all()
 
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d645341da3d5cd09006398538afc96c0'
-    
     weather_data = []
 
     for city in cities:
-        r = requests.get(url.format(city.name)).json()
+
+        r = get_weather_data(city.name)
+        print(r)
+
         weather = {
             'city' : city.name,
             'temperature' : r['main']['temp'],
             'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon']
+            'icon' : r['weather'][0]['icon'],
         }
 
         weather_data.append(weather)
@@ -43,12 +50,19 @@ def index_post():
     
     if new_city:
         existing_city = City.query.filter_by(name=new_city).first()
+
         if not existing_city:
-            new_city_obj = City(name=new_city)
-            db.session.add(new_city_obj)
-            db.session.commit()
+            new_city_data = get_weather_data(new_city)
+
+            if new_city_data['cod'] == 200:
+                new_city_obj = City(name=new_city)
+
+                db.session.add(new_city_obj)
+                db.session.commit()
+            else:
+                err_msg = 'City does not exist in the world!'
         else:
-            err_msg = 'City already exists!'
+            err_msg = 'City already exists in the database!'
 
 
 
